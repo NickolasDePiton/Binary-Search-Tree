0f3b6e086418217dc61238fb73d9d646ff133a83
NickolasDePiton <crysis4997@ya.ru>
2016-04-26 21:26:28 +0300

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -2,20 +2,20 @@
 #include <fstream>
 using namespace std;
 
-class BinarySearchTree{
+class BST {
 public:
-	BinarySearchTree() :root(nullptr){}
-	bool add(int x){
-		if (root == nullptr) { root = new der(x); return true; }
-		else { root->add(x); return true; }
+	BST() :parent(nullptr) {}
+	bool add(int x) { // Добавление элемента
+		if (parent == nullptr) { parent = new Tree(x); return true; }
+		else { parent->add(x); return true; }
 		return false;
 	}
-	bool search(int x){ return(root->search(x)); }
-	bool read_file(char* a){
+	bool search(int x) { return(parent->search(x)); } // Поиск
+	bool read_file(char* a) { // Чтение из файла
 		ifstream fin; int x;
 		fin.open(a);
-		if (fin.is_open()){
-			while (!fin.eof()){
+		if (fin.is_open()) {
+			while (!fin.eof()) {
 				fin >> x;
 				this->add(x);
 			}
@@ -24,84 +24,50 @@ public:
 		}
 		return false;
 	}
-	void print_file(char* b){
-		root->print_file(b);
+	void print_file(char* b) {
+		parent->print_file(b);
 	}
-	void print_console(){
-		root->print_console();
+	void print_console() {
+		parent->print_console();
 	}
 private:
-	class der{
+	class Tree {
 	public:
-		der(int x) :D(x), l(nullptr), r(nullptr){}
-		void add(int x){
-			if (x < D){ 
-				if (l != nullptr) l->add(x);
-				if (l == nullptr) l = new der(x); 
+		Tree(int x) :k(x), l(nullptr), r(nullptr) {}
+		void add(int x) { // Добавление элемента
+			if (x < k) { // Если нововведенный элемент x меньше чем элемент k из семечка дерева, уходим влево
+				if (l != nullptr) l->add(x); // Вставляем элемент на свободный участок
+				if (l == nullptr) l = new Tree(x); // Выделяем память левому подзвену
 			}
-			if (x>D){
+			if (x > k) { // Иначе идем вправо
 				if (r != nullptr) r->add(x);
-				if (r == nullptr) r = new der(x);
+				if (r == nullptr) r = new Tree(x);
 			}
 		}
-		bool search(int x){
-			if (x == D) return true;
-			if (x < D){ if (l != nullptr) l->search(x); else return false; }
-			if (x > D){ if (r != nullptr) r->search(x); else return false; }
+			bool search(int x){ // Поиск элемента
+			if (x == k) return true;
+			if (x < k){ if (l != nullptr) l->search(x); else return false; } // Идем в левую сторону и ищем элемент
+			if (x > k){ if (r != nullptr) r->search(x); else return false; } // Идем в правую
 			}
-		void print_console(){
-			if (l != nullptr) l->print_console();  
-			cout << D << " ";
-			if (r != nullptr) r->print_console();  
+		void print_console() {
+			if (l != nullptr) l->print_console();
+			cout << k << " ";
+			if (r != nullptr) r->print_console();
 		}
-		void print_file(char *b){
+		void print_file(char *b) {
 			ofstream fout;
 			fout.open(b, ios::app);
-			if (fout.is_open()){
+			if (fout.is_open()) {
 				if (l != nullptr) l->print_file(b);
-				fout << D << " "; fout.close();
+				fout << k << " "; fout.close();
 				if (r != nullptr) r->print_file(b);
 			}
 			fout.close();
 		}
 	private:
-		int D;
-		der *l;
-		der *r;
+		int k;
+		Tree *l;
+		Tree *r;
 	};
-	der* root;
+	Tree* parent;
 };
-
-
-
-int main(){
-	BinarySearchTree tree; int J;
-	cout << "add-1, search-2, read_f-3, print_f-4, print_c-5, exit-0\n";
-	do{
-		cout << "J="; cin >> J;
-		if (J == 1){
-			int x;
-			cout << "x="; cin >> x;
-			if(tree.add(x))cout<<"done\n";
-		}
-		if (J == 2){
-			int x;
-			cout << "x="; cin >> x;
-			if (tree.search(x)) cout << "founded\n"; else cout << "not founded\n";
-		}
-		if (J == 3){
-			char a[20];
-			cout << "name of file: "; cin >> a;
-			if (tree.read_file(a)) cout << "done\n";
-		}
-		if (J == 4){
-			char b[20];
-			cout << "name of file: "; cin >> b;
-			tree.print_file(b);
-		}
-		if (J == 5){
-			tree.print_console(); cout << endl;
-		}
-	} while (J);
-	system("pause");
-}
