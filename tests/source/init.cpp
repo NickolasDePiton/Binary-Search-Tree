ad2210f705dc6d9a6d363f28979f1e5ff191c13b
NickolasDePiton <crysis4997@ya.ru>
2016-05-05 20:32:46 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -6,32 +6,32 @@ using namespace std;
 
 SCENARIO("Add_int", "[add]") {
 	BST<int> tree;
-	REQUIRE(tree.add(7));
+	REQUIRE(tree.add(1));
+	REQUIRE(tree.add(2));
 	REQUIRE(tree.add(3));
-	REQUIRE(tree.add(5));
+	REQUIRE(tree.search(2));
 	REQUIRE(tree.search(3));
-	REQUIRE(tree.search(5));
-	REQUIRE(tree.search(7));
+	REQUIRE(tree.search(1));
 }
 
 SCENARIO("Add_char", "[add_c]") {
 	BST<char> tree;
+	REQUIRE(tree.add(6));
 	REQUIRE(tree.add(5));
 	REQUIRE(tree.add(4));
-	REQUIRE(tree.add(6));
-	REQUIRE(tree.search(4));
 	REQUIRE(tree.search(5));
 	REQUIRE(tree.search(6));
+	REQUIRE(tree.search(4));
 }
 
 SCENARIO("Add_double", "[add_d]") {
 	BST<double> tree;
-	REQUIRE(tree.add(7.62));
+	REQUIRE(tree.add(14.88));
 	REQUIRE(tree.add(3.14));
-	REQUIRE(tree.add(5.85));
+	REQUIRE(tree.add(2.17));
 	REQUIRE(tree.search(3.14));
-	REQUIRE(tree.search(5.85));
-	REQUIRE(tree.search(7.62));
+	REQUIRE(tree.search(2.17));
+	REQUIRE(tree.search(14.88));
 }
 
 SCENARIO("Search_int", "[search_i]") {
@@ -84,89 +84,89 @@ SCENARIO("Search_double", "[search_d]") {
 	REQUIRE(tree.search(5.85));
 	REQUIRE(tree.search(1.29));
 	REQUIRE(tree.search(3.14));
-	REQUIRE(!tree.search(0.5));
-	REQUIRE(!tree.search(8.800));
-	REQUIRE(!tree.search(6.666));
-	REQUIRE(!tree.search(4.13));
-	REQUIRE(!tree.search(2.34));
+	REQUIRE(!tree.search(4.3));
+	REQUIRE(!tree.search(2.777));
+	REQUIRE(!tree.search(14.888));
+	REQUIRE(!tree.search(6.22));
+	REQUIRE(!tree.search(8.14));
 }
 
 SCENARIO("Read_int", "[read_i]") {
 	BST<int> tree; ifstream fin("read.txt");
 	fin >> tree;
 	fin.close();
-	REQUIRE(tree.search(7));
-	REQUIRE(tree.search(9));
-	REQUIRE(tree.search(5));
 	REQUIRE(tree.search(1));
+	REQUIRE(tree.search(4));
 	REQUIRE(tree.search(3));
+	REQUIRE(tree.search(5));
+	REQUIRE(tree.search(2));
 }
 
 SCENARIO("Read_double", "[read_d]") {
 	BST<double> tree; ifstream fin("read_double.txt");
 	fin >> tree;
 	fin.close();
-	REQUIRE(tree.search(12.74));
-	REQUIRE(tree.search(15.62));
-	REQUIRE(tree.search(7.62));
+	REQUIRE(tree.search(14.88));
 	REQUIRE(tree.search(3.14));
-	REQUIRE(tree.search(8.34));
-	REQUIRE(tree.search(25.15));
-	REQUIRE(tree.search(17.16));
+	REQUIRE(tree.search(7.62));
+	REQUIRE(tree.search(2.30));
+	REQUIRE(tree.search(4.44));
+	REQUIRE(tree.search(5.02));
+	REQUIRE(tree.search(27.86));
 }
 
 SCENARIO("Print_int", "[print_file_i]") {
 	BST<int> tree, tree_2; ofstream fout("print.txt", ios::app);
-	tree.add(7);
+	tree.add(1);
+	tree.add(2);
 	tree.add(3);
-	tree.add(5);
 	fout << tree; fout << -1;
 	fout.close();
 	ifstream fin("print.txt");
 	fin >> tree_2;
 	fin.close();
-	REQUIRE(tree_2.search(7));
+	REQUIRE(tree_2.search(1));
+	REQUIRE(tree_2.search(2));
 	REQUIRE(tree_2.search(3));
-	REQUIRE(tree_2.search(5));
 }
 
 SCENARIO("Print_file_double", "[print_file_d]") {
 	BST<double> tree, tree_2; ofstream fout("print_double.txt", ios::app);
-	tree.add(7.77);
-	tree.add(3.33);
-	tree.add(5.55);
+	tree.add(1.23);
+	tree.add(4.56);
+	tree.add(7.89);
 	fout << tree; fout << -1;
 	fout.close();
 	ifstream fin("print_double.txt");
 	fin >> tree_2;
 	fin.close();
-	REQUIRE(tree_2.search(7.77));
-	REQUIRE(tree_2.search(3.33));
-	REQUIRE(tree_2.search(5.55));
+	REQUIRE(tree_2.search(1.23));
+	REQUIRE(tree_2.search(4.56));
+	REQUIRE(tree_2.search(7.89));
 }
 
 
 SCENARIO("Print_console_int", "[print_console_i]") {
 	BST<int> tree;
-	tree.add(7);
+	tree.add(1);
+	tree.add(2);
 	tree.add(3);
-	tree.add(5);
 	REQUIRE(cout << tree);
 }
 
 SCENARIO("Print_console_char", "[print_console_c]") {
 	BST<char> tree;
-	tree.add(7);
+	tree.add(1);
+	tree.add(2);
 	tree.add(3);
-	tree.add(5);
 	REQUIRE(cout << tree);
 }
 
 SCENARIO("Print_console_double", "[print_console_d]") {
 	BST<double> tree;
-	tree.add(7);
+	tree.add(1);
+	tree.add(2);
 	tree.add(3);
-	tree.add(5);
 	REQUIRE(cout << tree);
 }
 
