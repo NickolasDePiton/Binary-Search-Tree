96c0b1cc1c0b284cbb67f28210da71a5a83af98c
NickolasDePiton <crysis4997@ya.ru>
2016-05-05 20:16:42 +0300

    Update BST.h

--- include/BST.h
+++ include/BST.h
@@ -12,10 +12,11 @@ template <class T>
 ostream & operator<<(ostream & out, BST<T> &tree);
 
 template <class T>
-ofstream & operator<<(ofstream & fout, BST<T> &tree);
+ifstream & operator >> (ifstream & fin, BST<T> &tree);
 
 template <class T>
-ifstream & operator >> (ifstream & fin, BST<T> &tree);
+ofstream & operator<<(ofstream & fout, BST<T> &tree);
+
 class Exceptions {
 	char* err;
 public:
@@ -23,14 +24,14 @@ public:
 	char* what();
 };
 
-class Uzhe_est : public Exceptions {
-public:
-	Uzhe_est();
-};
-
 class File_Not_Open : public Exceptions {
 public:
 	File_Not_Open();
+};
+
+class Already_add : public Exceptions {
+public:
+	Already_add();
 };
 
 class Empty_tree : public Exceptions {
@@ -46,11 +47,11 @@ public:
 	bool add(T x);
 	bool search(T x);
 	friend ostream & operator<< <>(ostream &out, BST<T> &tree);
-	friend ofstream & operator<< <>(ofstream &fout, BST<T> &tree);
 	friend ifstream & operator>> <>(ifstream &fin, BST<T> &tree);
+	friend ofstream & operator<< <>(ofstream &fout, BST<T> &tree);
 private:
 	class Node;
-	Node* root;
+	Node* parent;
 };
 
 template <class T>
