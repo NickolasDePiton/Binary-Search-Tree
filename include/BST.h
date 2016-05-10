3881d7174b9f1c32003fa880139cc9d31ebb6988
NickolasDePiton <crysis4997@ya.ru>
2016-05-10 18:08:11 +0300

    Update BST.h

--- include/BST.h
+++ include/BST.h
@@ -39,6 +39,15 @@ public:
 	Empty_tree();
 };
 
+class Element_not_found : public Exceptions{  
+public:
+	Element_not_found();
+};
+
+class Tree_Was_Deleted : public Exceptions{  
+public:
+	Tree_Was_Deleted();
+};
 
 template <class T>
 class BST {
@@ -49,6 +58,8 @@ public:
 	friend ostream & operator<< <>(ostream &out, BST<T> &tree);
 	friend ifstream & operator>> <>(ifstream &fin, BST<T> &tree);
 	friend ofstream & operator<< <>(ofstream &fout, BST<T> &tree);
+	bool del(T x);
+	void Create_tree_again();
 private:
 	class Node;
 	Node* parent;
@@ -61,6 +72,8 @@ public:
 	void add(T x);
 	bool search(T x);
 	bool print_console();
+	void del(T x);
+	T min(der* M);
 	bool print_file(ofstream &fout);
 private:
 	T k;
